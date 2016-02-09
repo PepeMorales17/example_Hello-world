08eb56a363b47ca619acef42a8bf1159589ff23f
Jose Morales Lugo <josemoraleslugo@macbook-pro-de-jose.local>
2016-02-08 20:52:21 -0600

    Initial Commit

---
+++ pruebasEnEste/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebasEnEste
+//
+//  Created by Jose Morales Lugo on 2/8/16.
+//  Copyright © 2016 Jose Morales Lugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
